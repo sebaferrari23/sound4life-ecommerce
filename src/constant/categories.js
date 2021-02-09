8422ca646655841e97f2480b57ac6ad9e8996da4
sebaferrari23 <sebaferrari23@gmail.com>
2021-02-08 23:24:15 -0300

    Update assets

--- src/constant/categories.js
+++ src/constant/categories.js
@@ -1,12 +1,17 @@
+import EarbudsImage from '../images/category-earbuds.jpg';
+import HeadphonesImage from '../images/category-headphones.webp';
+
 export const categories = [
     {
         id: "1",
         title: "Headphones",
         route: '/category/headphones',
+        image: HeadphonesImage
     },
     {
         id: "2",
         title: "Earbuds",
         route: '/category/earbuds',
+        image: EarbudsImage
     },
 ];
